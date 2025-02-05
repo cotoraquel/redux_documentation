caa3a5a6b22f2e76282c9999cdf3883a7fee18dc
Raquel <cotoraquel@gmail.com>
2025-02-04 20:58:26 -0700

    Initial commit

---
+++ src/features/posts/PostsList.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+import { useAppSelector } from '@/app/hooks'
+import { selectAllPosts } from './postsSlice'
+import { PostAuthor } from './PostAuthor'
+export const PostsList = () => {
+  // Select the `state.posts` value from the store into the component
+  const posts = useAppSelector(selectAllPosts)
+
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  
+  const renderedPosts = posts.map(post => (
+    <article className="post-excerpt" key={post.id}>
+      <h3><Link to={`/posts/${post.id}`}>{post.title}</Link></h3>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+    </article>
+  ))
+
+  return (
+    <section className="posts-list">
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  )
+}
